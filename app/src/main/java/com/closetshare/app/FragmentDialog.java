e43807a8a1b448904cef1e4794bc200c57842596
Davee Nguyen <vibrantdavee@gmail.com>
2014-04-05 01:03:00 -0700

    Opens camera when click on take photo

--- app/src/main/java/com/closetshare/app/FragmentDialog.java
+++ app/src/main/java/com/closetshare/app/FragmentDialog.java
@@ -6,14 +6,20 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Toast;
+
+import java.io.File;
 
 
 /**
  * A simple {@link android.support.v4.app.Fragment} subclass.
  */
 public class FragmentDialog extends DialogFragment {
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -26,8 +32,10 @@ public class FragmentDialog extends DialogFragment {
                         switch (which) {
                             case 0: {
                                 Toast.makeText(FragmentDialog.this.getActivity(), which + " was clicked", Toast.LENGTH_SHORT).show();
-                                Intent i = new Intent(getActivity(), AddItemActivity.class);
-                                startActivity(i);
+                                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                                i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                                startActivityForResult(i, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
                                 break;
                             }
                             case 1: {
