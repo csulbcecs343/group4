525a1fdbc66a4d5c38b0a88f7c6a1c8ac3cedd25
Albert <albertv714@gmail.com>
2014-04-14 21:11:41 -0700

    view item working

--- app/src/main/java/com/closetshare/app/ViewItemActivity.java
+++ app/src/main/java/com/closetshare/app/ViewItemActivity.java
@@ -1,9 +1,11 @@
 package com.closetshare.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class ViewItemActivity extends Activity {
@@ -12,6 +14,7 @@ public class ViewItemActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_item);
+
     }
 
 
