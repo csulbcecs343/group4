b4b281f716a69c51c490c1870cd49d7e8a6f8521
Davee Nguyen <vibrantdavee@gmail.com>
2014-04-15 19:25:47 -0700

    Forgot import when handling merge conflict

--- app/src/main/java/com/closetshare/app/FragmentCloset.java
+++ app/src/main/java/com/closetshare/app/FragmentCloset.java
@@ -3,6 +3,7 @@ package com.closetshare.app;
 
 import android.app.DialogFragment;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
