ce2e0a4561f2c42750b3b7ee0a3447288fd120e5
Davee Nguyen <vibrantdavee@gmail.com>
2014-04-18 19:19:37 -0700

    receiving the query

--- app/src/main/java/com/closetshare/app/SearchableActivity.java
+++ app/src/main/java/com/closetshare/app/SearchableActivity.java
@@ -1,15 +1,31 @@
 package com.closetshare.app;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class SearchableActivity extends Activity {
+public class SearchableActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchable);
+
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+
+    /**
+     * Method where the actual search is done.
+     * @param query is the search string.
+     */
+    private void doMySearch(String query) {
+
     }
 }
