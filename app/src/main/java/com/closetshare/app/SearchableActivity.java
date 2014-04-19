83249fba9e60e07efe36b3ab5667f5a0670e607e
Davee Nguyen <vibrantdavee@gmail.com>
2014-04-18 18:08:56 -0700

    Created searchable activity

---
+++ app/src/main/java/com/closetshare/app/SearchableActivity.java
@@ -0,0 +1,15 @@
+package com.closetshare.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SearchableActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchable);
+    }
+}
